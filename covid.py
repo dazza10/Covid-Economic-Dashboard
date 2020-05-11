d2226f0098aa4bf1597f503b1a078eb73f6978f2
dazza10 <46102999+dazza10@users.noreply.github.com>
2020-05-11 15:12:34 +0800

    Create covid.py

---
+++ covid.py
@@ -0,0 +1,9 @@
+#importing all packages
+import requests
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import StringIO
+from matplotlib import dates as mpl_dates
