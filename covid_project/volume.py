65484db5a72ab642700ff7db416a0c0570f10374
dazza10 <dharrankandaiah@gmail.com>
2020-05-29 15:14:54 +0800

    update table name top active

--- covid_project/volume.py
+++ covid_project/volume.py
@@ -35,5 +35,5 @@ conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser,secrets.dbpass,se
 engine = sqlalchemy.create_engine(conn)
 
 # Moving the the Data Base
-df.to_sql(name="Top_Active",con=engine,index=False,if_exists='append')
+df.to_sql(name="top_active",con=engine,index=False,if_exists='append')
 
